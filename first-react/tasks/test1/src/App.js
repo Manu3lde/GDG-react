fe692c81b3f155d71841aa0d70d34ad89aae0ce2
Manuel <blackonyxx20.4@gmail.com>
2025-11-27 19:24:34 +0300

    first react changes

---
+++ first-react/tasks/test1/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import UserCard from './UserCard';
+
+function App() {
+  //const members = [];
+  
+
+  return (
+    <div className="App">
+        {/*<GroupMembers members={members} />*/}
+        <UserCard />
+      </div>
+  );
+}
+
+export default App;
